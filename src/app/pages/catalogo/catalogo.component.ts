821b39a051787edc05d98fff743778c472e649a1
BrayanCristhian <brayanreiter98@gmail.com>
2024-08-06 21:57:04 -0300

    hot wheels

---
+++ src/app/pages/catalogo/catalogo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogo',
+  templateUrl: './catalogo.component.html',
+  styleUrl: './catalogo.component.scss'
+})
+export class CatalogoComponent {
+cars: any;
+highlights: any;
+
+}
