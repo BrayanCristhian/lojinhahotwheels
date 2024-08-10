a96949564a5da8f4e2cc48fbc98c56d496ec7d4e
BrayanCristhian <brayanreiter98@gmail.com>
2024-08-09 21:51:16 -0300

    aba_de_login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CatalogoComponent
+    CatalogoComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
